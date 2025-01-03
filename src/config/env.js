1d78912925f6fc350334a0ec43352375701a530e
이석주 <nick981213@naver.com>
2025-01-03 23:51:37 +0900

    config 폴더 내의 db.js, env.js 파일 생성성

---
+++ src/config/env.js
@@ -0,0 +1,5 @@
+module.exports = {
+    PORT: process.env.PORT || 3000, // 환경 변수 PORT 또는 기본값 3000
+    // DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost:27017/mydb',
+    // JWT_SECRET: process.env.JWT_SECRET || 'default_secret_key',
+};
