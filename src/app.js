b8bcdf74743631992e844b1be6756bdb7eae8591
이석주 <nick981213@naver.com>
2025-01-06 22:12:21 +0900

    - 'service' folder name changed into 'services' - app.js, server.js is modifed

--- src/app.js
+++ src/app.js
@@ -1,17 +1,13 @@
 const express = require("express")
-const routes = require("./routes")
-const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 
 // 미들웨어 설정
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-// 라우터 연결
-app.use('/api', routes);
-
-// 에러 핸들러
-app.use(errorHandler);
+// 라우터 설정
+app.get("/", (req, res) => {
+    res.send("Hello, Express!");
+});
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const app = require("./app")
-const { PORT } = require('./config/env');
+const { PORT } = require("./config/env"); // 환경 변수에서 PORT 가져오기
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
