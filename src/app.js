0e522df7234af43ac0ef588dc2c561f32cf3db83
이석주 <nick981213@naver.com>
2025-01-12 16:58:53 +0900

    - docker hub webhook을 위한 webhook.js  작성

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const userRoutes = require("./routes/users");
 const todoRoutes = require("./routes/todos");
+const scheduleRoutes = require("./routes/schedules");
+const webhookRoutes = require("./webhook");
 require("dotenv").config();
 
 const app = express();
@@ -11,6 +13,10 @@ app.use(express.json());
 // 라우터 설정
 app.use("/api/users", userRoutes);
 app.use("/api/todos", todoRoutes);
+app.use("/api/schedules", scheduleRoutes);
+
+// 웹 훅 라우터 연결
+app.use("/", webhookRoutes);
 
 // 기본 경로
 app.get("/", (req, res) => {
